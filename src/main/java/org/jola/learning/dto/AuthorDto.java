821e49478b4f90520b419c3f4d6b9cc1c9f1344d
GitUserJoLa <josephine.langer@gmx.de>
2024-07-25 12:21:48 +0200

    add comments concerning field constraints for data table construction

--- src/main/java/org/jola/learning/dto/AuthorDto.java
+++ src/main/java/org/jola/learning/dto/AuthorDto.java
@@ -17,23 +17,29 @@ public class AuthorDto {
 
     @NotNull
     @Setter(AccessLevel.NONE)
+    //primary key, bigint, not null, autoincrement, autogenerate
     private Long id;
 
     @NotNull
+    //string, not null
     private String firstName;
 
     @NotNull
+    //string, not null
     private String lastName;
 
     @NotNull
+    //string, not null, unique
     private String alias;
 
     @NotNull
     @Setter(AccessLevel.NONE)
+    //string, not null, unique
     private String email;
 
     @NotNull
     @Getter(AccessLevel.NONE)
+    //hashed string, not null
     // should also be stored hashed in db and never be clear text
     private String password;
 }
--- src/main/java/org/jola/learning/model/Author.java
+++ src/main/java/org/jola/learning/model/Author.java
@@ -15,10 +15,17 @@ import java.io.Serializable;
 @Setter
 public class Author implements Serializable {
 
+    //primary key, bigint, not null, autoincrement, autogenerate
     private Long id;
+    //string, not null
     private String firstName;
+    //string, not null
     private String lastName;
+    //string, not null, unique
     private String alias;
+    //string, not null, unique
     private String email;
+    //hashed string, not null
+    // should also be stored hashed in db and never be clear text
     private String password;
 }
